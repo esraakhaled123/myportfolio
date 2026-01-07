091dfe928db04f4dc5514a9cfc381ed4cda416dd
esraa khaled <esraakhaledharfoush@gmail.com>
2026-01-07 03:13:34 +0200

    your message

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ const FEATURED_PROJECTS = [
     tech: ["Next.js", "TypeScript", "Tailwind CSS"],
     image:
       "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=800&h=500&fit=crop",
-    demo: "https://e-commerce-five-phi-93.vercel.app/",
+    demo: "https://scocial-platform-t7tu.vercel.app/login",
     github: "https://github.com/esraakhaled123/e-commerce.git",
   },
   {
